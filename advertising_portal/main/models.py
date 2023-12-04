60b820999315df8836606ef725defe7647e9f8f9
MateuszSamluk <mat.sam00@wp.pl>
2023-12-04 20:45:52 +0100

    remove wrong project

--- advertising_portal/main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Offer(models.Model):
-    id = models.AutoField(primary_key=True, null=False, blank=False)
-    title = models.CharField(max_length=150)
-    content = models.TextField(max_length = 10000)
